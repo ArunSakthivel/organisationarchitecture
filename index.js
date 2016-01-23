9ad24571be8ab9da6e62a2d2e1e2811751291a22
ArunSakthivel <sarun_5007@hotmail.com>
2016-01-23 20:01:31 +0530

    modified indexjs

--- index.js
+++ index.js
@@ -27,11 +27,11 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('index')
+  response.render('./index.html')
 });
 
 app.get('/cool', function(request, response) {
